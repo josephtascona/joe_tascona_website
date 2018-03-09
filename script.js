fa4facd349b326d9690e0d94d1980d3e01690c11
Joseph Tascona <josephtascona@Josephs-MacBook-Pro.local>
2018-03-08 23:29:53 -0400

    Improved speed of site

--- script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-  document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
-}
-
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
